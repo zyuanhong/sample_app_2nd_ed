651f8a45a88279f4c6f239bcef0429786b6d0078
Yuan-Hong Zeng <zmetahong@gmail.com>
2013-04-27 20:33:32 +0800

    An (unsuccessful) attempt at handling failed signin.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:Session][:password])
+      # Sign the user in and redirect to the user's show page.
+    else
+      flash[:error] = 'Invalid eamil/password combination' # Not quite right!
+      render 'new'
+    end
   end
 
   def destroy
