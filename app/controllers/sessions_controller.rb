952d82ae2e8159a0d211028ff80b1b55b6bc99a6
Yuan-Hong Zeng <zmetahong@gmail.com>
2013-04-27 16:38:00 +0800

    A preliminary version of the Sessions create action.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,11 @@
 class SessionsController < ApplicationController
-  def new;     end
-  def create;  end
-  def destroy; end
+  def new
+  end
+
+  def create
+    render 'new'
+  end
+
+  def destroy
+  end
 end
