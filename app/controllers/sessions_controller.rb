15bf73eaf67e6500de813a5b0bbb98f7cfc15be3
Yuan-Hong Zeng <zmetahong@gmail.com>
2013-05-02 21:49:33 +0800

    [Fix] Fix a typo in 'SessionsController' and 'authentication_pages_apec'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:session][:email].downcase)
-    if user && user.authenticate(params[:Session][:password])
+    if user && user.authenticate(params[:session][:password])
       sign_in user
       redirect_to user
     else
