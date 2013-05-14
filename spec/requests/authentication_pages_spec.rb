098979c8ec63c013ae20f97181eae0650b3ee9b2
Yuan-Hong Zeng <zmetahong@gmail.com>
2013-05-14 17:26:31 +0800

    [Add] A test for the "Users" link URI.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -30,10 +30,13 @@ describe "Authentication" do
       let(:user) { FactoryGirl.create(:user) }
       before { sign_in(user) }
 
-      it { should have_selector('title',   :text => user.name) }
-      it { should have_link('Profile',     :href => user_path(user)) }
-      it { should have_link('Settings',    :href => edit_user_path(user)) }
-      it { should have_link('Sign out',    :href => signout_path) }
+      it { should have_selector('title', :text => user.name) }
+
+      it { should have_link('Users',    :href => users_path) }
+      it { should have_link('Profile',  :href => user_path(user)) }
+      it { should have_link('Settings', :href => edit_user_path(user)) }
+      it { should have_link('Sign out', :href => signout_path) }
+
       it { should_not have_link('Sign in', :href => signin_path) }
 
       describe "followed by signout" do
