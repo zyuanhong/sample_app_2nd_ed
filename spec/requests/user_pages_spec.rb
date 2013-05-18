9eb80989732a6a3f222375c6f98599a030d73f93
Yuan-Hong Zeng <zmetahong@gmail.com>
2013-05-18 21:47:48 +0800

    [Add] Tests for delete links.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,7 +8,7 @@ describe "User pages" do
 
     let(:user) { FactoryGirl.create(:user) }
 
-    before(:each) do
+    before do
       sign_in user
       visit users_path
     end
@@ -27,6 +27,25 @@ describe "User pages" do
         User.paginate(:page => 1).each do |user|
           page.should have_selector('li', :text => user.name)
         end
+      end
+    end
+
+    describe "delete link" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', :href => user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', :href => user_path(admin)) }
       end
     end
   end
