afe6378bed48c9e0d7a53cba6f57704b4af126bf
Yuan-Hong Zeng <zmetahong@gmail.com>
2013-05-20 16:28:48 +0800

    [Add] Add tests to make sure that these links dont't appear when a user isn't signed in.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -5,10 +5,16 @@ describe "Authentication" do
   subject { page }
 
   describe "signin page" do
+    let(:user) { FactoryGirl.create(:user) }
     before { visit signin_path }
 
     it { should have_selector('h1',    :text => 'Sign in') }
     it { should have_selector('title', :text => 'Sign in') }
+
+    it { should_not have_link('Users',    :href => users_path) }
+    it { should_not have_link('Profile',  :href => user_path(user)) }
+    it { should_not have_link('Settings', :href => edit_user_path(user)) }
+    it { should_not have_link('Sign out', :href => signout_path) }
   end
 
   describe "signin" do
