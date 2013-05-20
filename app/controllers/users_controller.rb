4a78d968b1914e6b23e861a303d07bef1f01a786
Yuan-Hong Zeng <zmetahong@gmail.com>
2013-05-20 22:53:54 +0800

    [Mod] In the controller use *_url instead of *_path.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,11 +62,11 @@ class UsersController < ApplicationController
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
+      redirect_to root_url unless current_user?(@user)
     end
 
     def admin_user
-      redirect_to(root_path) unless current_user.admin?
+      redirect_to root_url unless current_user.admin?
     end
 
     def already_login
