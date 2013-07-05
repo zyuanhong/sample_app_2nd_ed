a3636a33242b5f19d71677d547a6a2fb7cf1ec71
Yuan-Hong Zeng <zmetahong@gmail.com>
2013-07-05 18:29:58 +0800

    [Add] The final implementation of from_users_followed_by.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -14,15 +14,15 @@ class Micropost < ActiveRecord::Base
 
   belongs_to :user
 
-  validates :content, :presence => true, :length => { :maximum => 140 }
   validates :user_id, :presence => true
+  validates :content, :presence => true, :length => { :maximum => 140 }
 
   default_scope :order => 'microposts.created_at DESC'
 
-  # Returns microposts from the users begin followed by the given user.
   def self.from_users_followed_by(user)
-    followed_user_ids = user.followed_user_ids
-    where("user_id IN (:followed_user_ids) OR user_id = :user_id",
-          :followed_user_ids => followed_user_ids, :user_id => user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+          :user_id => user.id)
   end
 end
